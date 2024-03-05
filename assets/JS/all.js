d29f6dfc3da6e70643bcca2570dc3e00ddc80313
ztionbar <ztion1990@gmail.com>
2024-03-06 00:28:48 +0800

    修改 index 架構及新增 all.js 內容

--- assets/JS/all.js
+++ assets/JS/all.js
@@ -1,65 +1,27 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const decreaseButton = document.getElementById('decrease');
-//     const increaseButton = document.getElementById('increase');
-//     const quantityElement = document.getElementById('quantity');
+document.addEventListener('DOMContentLoaded', function () {
+    const decreaseButton = document.querySelector('.decrease-btn');
+    const increaseButton = document.querySelector('.increase-btn');
+    const quantityElement = document.querySelector('#quantity');
+  console.log(decreaseButton);
+    let quantity = 0;
   
-//     let quantity = 0;
+    function updateQuantity() {
+      quantityElement.textContent = quantity;
+    }
   
-//     function updateQuantity() {
-//       quantityElement.textContent = quantity;
-//     }
+    decreaseButton.addEventListener('click', function () {
+      if (quantity > 0) {
+        quantity--;
+        updateQuantity();
+      }
+    });
   
-//     decreaseButton.addEventListener('click', function () {
-//       if (quantity > 0) {
-//         quantity--;
-//         updateQuantity();
-//       }
-//     });
-  
-//     increaseButton.addEventListener('click', function () {
-//       quantity++;
-//       updateQuantity();
-//     });
-//   });
+    increaseButton.addEventListener('click', function () {
+      quantity++;
+      updateQuantity();
+    });
+  });
 
-//   document.addEventListener('DOMContentLoaded', function () {
-//         init();
-//     });
-
-
-//     document.addEventListener('DOMContentLoaded', function () {
-//       initializeCounter('.quantity-counter');
-//     });
-//     console.log("DOMContentLoaded");
-//     function initializeCounter(selector) {
-//     const blocks = document.querySelectorAll(selector);
-//     console.log(blocks);
-//     console.log(blocks);
-//     blocks.forEach(function (block) {
-//         console.log(block);
-//       const decreaseButton = block.querySelector('.decrease');
-//       console.log(decreaseButton);
-//       const increaseButton = block.querySelector('.increase');
-//       const quantityElement = block.querySelector('.quantity');
-//       console.log(quantityElement);
-//       let quantity = 0;
-//         console.log(blocks)
-//       function updateQuantity() {
-//         quantityElement.textContent = quantity;
-//       }
-  
-//       decreaseButton.addEventListener('click', function () {
-//         if (quantity > 0) {
-//           quantity--;
-//           updateQuantity();
-//         }
-//       });
-  
-//       increaseButton.addEventListener('click', function () {
-//         quantity++;
-//         updateQuantity();
-//       });
-//     });
-//   }
-  
-  
+  document.addEventListener('DOMContentLoaded', function () {
+        init();
+    });
